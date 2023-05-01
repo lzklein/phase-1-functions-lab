902879b2f54e11b8edf66190b1371820ba4bc66e
Louis Klein <louisklein71@gmail.com>
2023-05-01 09:49:26 -0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+var street;
+
+function distanceFromHqInBlocks(street){return Math.abs(42 - street)}
+function distanceFromHqInFeet(street){return distanceFromHqInBlocks(street)*264}
+var start;
+var destination;
+function distanceTravelledInFeet(start, destination){return Math.abs(start - destination)*264}
+function calculatesFarePrice(start, destination){
+    if (Math.abs(start - destination)*264 <= 400){return 0}
+    else if (Math.abs(start - destination)*264 <= 2000){return (Math.abs(distanceTravelledInFeet(start, destination)-400)*.02)}
+    else if (Math.abs(start - destination)*264 <= 2500){return 25}
+    else {return 'cannot travel that far'}
+}
